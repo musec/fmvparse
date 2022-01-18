2eca2b0b99aaa249c57618a4a1af15f2d69b879b
arastoob <a.bozorgi67@gmail.com>
2022-01-18 12:13:50 -0330

    Use StructOps to handle the inputs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,18 +1,33 @@
+
+extern crate structopt;
+
 use fmvparse::reader;
-use std::env;
 use std::fs::File;
 use std::io;
 use std::io::BufReader;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+name = "fmv-parser",
+about = "Parse fmv files and extract the internal structures."
+)]
+struct Opt {
+    #[structopt(short, long)]
+    verbose: bool,
+
+    #[structopt(short, long, env = "FMV_FILE")]
+    /// The input file to be parsed
+    file: PathBuf,
+}
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: {} <filename>", args[0]);
-    }
-    let path = &args[1];
-    // --snip--
-    println!("In file: {}", path);
-    let file = File::open(path).expect("Something went wrong reading the file");
+    let opt = Opt::from_args();
+
+    println!("In file: {:?}", opt.file);
+    let file = File::open(&opt.file).expect("Something went wrong reading the file");
+
     let mut reader = BufReader::new(file);
     let mut writer: Vec<u8> = vec![];
     io::copy(&mut reader, &mut writer)?;
